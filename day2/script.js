2c8b06eadd2cee3cc0589ae80e751ae3ca791405
pattcaroline <43247066+pattcaroline@users.noreply.github.com>
2026-03-11 11:14:33 -0400

    Add files via upload
    Progress bar project

---
+++ day2/script.js
@@ -0,0 +1,56 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+//Previous and Next button with event listener and function UPDATE()
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+// This function add or removes the actice class from the circles if currentActive is incremented or decremented
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  // Adding style to the progress line
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  //console.log("actives length: " + actives.length);
+  //console.log("circles length: " + circles.length);
+
+  // DISABLED buttons when circle is at the start position (1) and at the end (4)
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
