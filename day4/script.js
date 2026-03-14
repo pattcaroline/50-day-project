896fd9025f91d290381213293b499dd17644b910
pattcaroline <43247066+pattcaroline@users.noreply.github.com>
2026-03-14 08:57:50 -0400

    Add files via upload

--- day4/script.js
+++ day4/script.js
@@ -1,7 +1,11 @@
-const search = document.querySelector(".search");
-const btn = document.querySelector(".btn");
-const input = document.querySelector(".input");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
 
-btn.addEventListener("click", () => {
-  search.classList.add(".active");
+open.addEventListener("click", () => {
+  container.classList.add("show-nav");
+});
+
+close.addEventListener("click", () => {
+  container.classList.remove("show-nav");
 });
