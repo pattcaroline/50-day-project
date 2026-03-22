ba01437f0fb8b22caad24bf04cc7cdac7a9b7f99
pattcaroline <43247066+pattcaroline@users.noreply.github.com>
2026-03-22 06:01:56 -0400

    Add files via upload

--- day13/script.js
+++ day13/script.js
@@ -1,1 +1,73 @@
- 
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  //Call randomSelect after user hits ENTER
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  const times = 30; //number of times it will highlight the choices before chosing a choice
+
+  //Highlighting and Unhighlight the tags
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    if (randomTag !== undefined) {
+      highlightTag(randomTag);
+      setTimeout(() => {
+        unHighlightTag(randomTag);
+      }, 100);
+    }
+  }, 100);
+
+  //Picking ONE tag to HIGHLIGHT after selecting the randomTag
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+//return Nodelist selected element
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
