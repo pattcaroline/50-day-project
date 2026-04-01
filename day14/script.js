aecf8dd73e880fbf9f91e404746cef17b09d1839
pattcaroline <43247066+pattcaroline@users.noreply.github.com>
2026-04-01 10:58:34 -0400

    Add files via upload

--- day14/script.js
+++ day14/script.js
@@ -1,1 +1,6 @@
- 
+const toggleBtn = document.getElementById("toggleBtn");
+const nav = document.getElementById("nav");
+
+toggleBtn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
