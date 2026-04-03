6934549e77e38c0aca97cfcf55a2f64d8a344b44
pattcaroline <43247066+pattcaroline@users.noreply.github.com>
2026-04-03 08:31:37 -0400

    Add files via upload

--- day15/script.js
+++ day15/script.js
@@ -1,1 +1,26 @@
- 
+const counters = document.querySelectorAll(".counter");
+
+// Setting counter to 0
+counters.forEach((counter) => {
+  counter.innerHTML = "0";
+
+  const updateCounter = () => {
+    //getting the counter value (0) and updating the value in the innerText
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+
+    //How fast to increment the number until the data-target (12000, 5000, 7500)
+    const increment = target / 50;
+
+    //If the value of c is still less than the target value, keep incrementing
+    //Math.ceil to round up the number
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 50);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
